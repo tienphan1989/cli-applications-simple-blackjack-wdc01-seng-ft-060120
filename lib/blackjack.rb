013fe43b3d3eaac7385dabae27d14421812969fd
Tien Phan <tienphan1989@gmail.com>
2020-05-07 06:49:25 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -37,32 +37,19 @@ def initial_round
 end
 
 def hit?(total)
-  # code hit? here
   prompt_user
   input = get_user_input
   if input == 's'
     total
-    else if input == 'h'
+    elsif input == 'h'
       total += deal_card
-      else        invalid_command
+      else invalid_command
         prompt_user
   end
   total
 end
 
-def hit? (num)
-   prompt_user
-   input = get_user_input #get the value from the user input
-   if input == "s"
-      num
-   elsif input == "h"
-      num += deal_card
-   else
-      invalid_command
-      prompt_user
-   end
-   num #return total number after the user makes a choice
-end
+
 
 def invalid_command
   # code invalid_command here
